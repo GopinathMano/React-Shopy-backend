1a508b0d32dc819e0a174c25c16c3aa4419084a6
Gopinath Mano <gopimano4242@gmail.com>
2021-12-09 19:43:15 +0530

    code changed

--- backend/server.js
+++ backend/server.js
@@ -3,26 +3,26 @@ import mongoose from "mongoose";
 import cors from "cors";
 import userRouter from "./routers/userRouter.js";
 import productRouter from "./routers/productRouter.js";
-import orderRouter from './routers/orderRouter.js';
+import orderRouter from "./routers/orderRouter.js";
 import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // mongoose connection
-mongoose.connect(process.env.MONGODB_URL , {
+mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 // sever middleware
 
-app.use(cors());
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
-app.use('/api/orders', orderRouter);
+app.use("/api/orders", orderRouter);
 
 // server Running notification
 app.get("/", (req, res) => {
@@ -35,7 +35,7 @@ app.get("/", (req, res) => {
 // });
 
 // server listener
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`server is running in port ${port}`);
 });
